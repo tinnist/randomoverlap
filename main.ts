cb8b26f7e1eae4a24403781bd0d5b227bef79176
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-15 13:29:27 -0400

    Random Overlap Example

--- main.ts
+++ main.ts
@@ -1,1 +1,48 @@
-
+namespace SpriteKind {
+    export const hat = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setPosition(randint(15, 145), randint(15, 105))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.hat, function (sprite, otherSprite) {
+    mySprite.say("Excuse me!", 500)
+})
+let mySprite: Sprite = null
+let hat = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 3 3 3 3 . . . . . 
+    . . . . . . . 3 . . 3 . . . . . 
+    . . . . . . . 3 . . 3 . . . . . 
+    . . . . . . . 3 . . 3 . . . . . 
+    . . . . . . . 3 . . 3 . . . . . 
+    . . . . . . . 3 . . 3 . . . . . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.hat)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 5 5 5 5 . . . . . . 
+    . . . . . . 5 . . 5 5 . . . . . 
+    . . . . . . 5 . . . 5 . . . . . 
+    . . . . . . 5 . . . 5 . . . . . 
+    . . . . . . 5 5 5 5 5 . . . . . 
+    . . . . . . . . 5 5 . . . . . . 
+    . . . . . . . . 5 . . . . . . . 
+    . . . . . . . 5 5 5 . . . . . . 
+    . . . . . 5 5 5 5 5 5 5 . . . . 
+    . . . 5 5 5 . . 5 . . 5 5 5 . . 
+    . . . . . . . . 5 . . . . . . . 
+    . . . . . . 5 5 5 5 5 5 . . . . 
+    . . . . . . 5 . . . . 5 . . . . 
+    . . . . . . 5 . . . 5 5 . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(randint(15, 145), randint(15, 105))
+hat.setPosition(35, 60)
