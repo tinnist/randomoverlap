a93ce15dcf4a1a2a561b22e9e8da8dcef4e4ac58
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-15 13:32:07 -0400

    random overlap task

--- main.ts
+++ main.ts
@@ -3,12 +3,19 @@ namespace SpriteKind {
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setPosition(randint(15, 145), randint(15, 105))
+    hat3.setPosition(randint(15, 145), randint(15, 105))
+    hat.setPosition(randint(15, 145), randint(15, 105))
+    hat2.setPosition(randint(15, 145), randint(15, 105))
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.hat, function (sprite, otherSprite) {
     mySprite.say("Excuse me!", 500)
+    mySprite.setVelocity(randint(15, 145), randint(15, 145))
 })
+let hat3: Sprite = null
+let hat2: Sprite = null
 let mySprite: Sprite = null
-let hat = sprites.create(img`
+let hat: Sprite = null
+hat = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . 3 3 3 3 . . . . . 
@@ -46,3 +53,41 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 mySprite.setPosition(randint(15, 145), randint(15, 105))
 hat.setPosition(35, 60)
+hat2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 2 2 2 2 2 2 2 2 . . 
+    . . . . . . 2 . . . . . . 2 2 . 
+    . . . . . . 2 2 2 . . . . . 2 . 
+    . . . . . . . . 2 2 2 2 2 2 2 . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.hat)
+hat3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . 4 4 4 4 . . . . . . . 
+    . . . . . 4 . . 4 4 4 . . . . . 
+    . . . . . 4 . . . . 4 . . . . . 
+    . . . . . 4 . . . . 4 . . . . . 
+    . . . . . 4 . . . . 4 . 4 . . . 
+    . . 4 . . 4 . . . . 4 . 4 . . . 
+    . . 4 4 . 4 . . . 4 4 . 4 . . . 
+    . . . 4 . 4 . . . 4 . . 4 . . . 
+    . . . 4 . 4 . . . 4 . 4 . . . . 
+    . . . . 4 4 . . . 4 4 4 . . . . 
+    . . . . 4 4 . . . 4 4 . . . . . 
+    . . . . . . 4 4 4 4 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.hat)
+hat2.setPosition(randint(15, 145), randint(15, 105))
+hat3.setPosition(randint(15, 145), randint(15, 105))
